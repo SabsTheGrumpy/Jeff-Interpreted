fb29cedbc695b1c2df490bfcfdd5f9501cfcb026
Grumpy <thesabatuer@gmail.com>
2024-04-30 22:32:07 -0700

    Add eval of functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -37,8 +37,23 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		env.Set(node.Name.Value, val)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
 
 	// Expressions
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+
+		return applyFunction(function, args)
 	case *ast.Indentifier:
 		return evalIdentifier(node, env)
 
@@ -71,6 +86,54 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return evalIfExpression(node, env)
 	}
 	return nil
+}
+
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+
+	return result
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(
+	fn *object.Function,
+	args []object.Object,
+) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.Return); ok {
+		fmt.Println(returnValue.Value)
+		return returnValue.Value
+	}
+
+	return obj
 }
 
 func evalIdentifier(node *ast.Indentifier, env *object.Environment) object.Object {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -276,6 +276,55 @@ func TestLetStatements(t *testing.T) {
 
 }
 
+
+func TestFunctionObject(t *testing.T) {
+	input := "fn(x) {  x + 2 ; };"
+
+
+	evaluated := testEval(input)
+	fn, ok := evaluated.(*object.Function)
+
+	if !ok {
+		t.Fatalf("Evaluated %T(%+v) could not be converted to function", evaluated, evaluated)
+	}
+
+
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("Expected lengh of parameters to be 1, but got %d", len(fn.Parameters))
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("Expected function parameter to be x but got %s", fn.Parameters[0])
+	}
+
+	expectedBody := "(x + 2)"
+
+	if fn.Body.String() != expectedBody {
+		t.Fatalf("Expected function body to be %s but got %s", expectedBody, fn.Body)
+	}
+}
+
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input string
+		expected int64
+	}{
+		{"let identity = fn(x) { x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x; }; identity(5);", 5},
+		{"let double = fn(x) { x * 2; }; double(5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5, 5);", 10},
+		{"let add = fn(x, y) { x + y; }; add(5 + 5, add(5, 5));", 20},
+		{"fn(x) { x; }(5)", 5},
+	}
+
+	for _, testCase := range tests {
+		testIntegerObject(t, testEval(testCase.input), testCase.expected)
+	}
+}
+
+
+
 func testNullObject(t *testing.T, obj object.Object) bool {
 
 	if obj != NULL {
